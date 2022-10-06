273e619f9ca30f7f54f22a399994201b6c28fd8f
robsonffsantos <robson.2501@hotmail.com>
2022-10-06 17:52:45 -0300

    config: koa router config

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 import Koa from 'koa'
+import Router from "@koa/router"
 
 const app = new Koa()
+const router = new Router()
 
-app.use(async ctx => {
+router.get('/', async ctx => {
   ctx.body = 'Hello World'
 })
 
+app.use(router.routes())
+app.use(router.allowedMethods())
 app.listen(3000)
