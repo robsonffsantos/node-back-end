17a0d8fafa8905d573b2461bf1f2c50649afa85e
robsonffsantos <robson.2501@hotmail.com>
2022-10-06 23:54:34 -0300

    config: install koa cors

--- api/setup.js
+++ api/setup.js
@@ -1,7 +1,10 @@
 import Koa from 'koa'
 import { router } from './router.js'
+import bodyParser from 'koa-bodyparser'
 
 export const app = new Koa()
+
+app.use(bodyParser())
 
 app.use(router.routes())
 app.use(router.allowedMethods())
