68f7a362bf3623eacebf340f0f350d6eb55b9372
robsonffsantos <robson.2501@hotmail.com>
2022-10-07 11:46:56 -0300

    config: adjust get function

--- api/characters/index.js
+++ api/characters/index.js
@@ -3,5 +3,14 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export const getChar = async (ctx) => {
-
+    try{
+        const char = await prisma.char.findMany()
+        ctx.body = char
+        ctx.status = 200
+    } catch (error) {
+        ctx.body = error
+        ctx.status = 500
+        console.log(error)
+    }
+    
 }
--- api/router.js
+++ api/router.js
@@ -1,24 +1,6 @@
 import Router from "@koa/router"
-import { PrismaClient } from '@prisma/client'
+import * as characters from './characters/index.js'
 
-const prisma = new PrismaClient()
 export const router = new Router()
 
-const characters = []
-
-router.get('/characters', async ctx => {
-    ctx.body = characters
-})
-
-router.post('/characters', async ctx => {
-    const char = {
-        photo: ctx.request.body.photo,
-        firstName: ctx.request.body.firstName,
-        lastName: ctx.request.body.lastName,
-        email: ctx.request.body.email,
-    }
-
-    characters.push(char)
-
-    ctx.body = char
-})
+router.get('/characters', characters.getChar)
